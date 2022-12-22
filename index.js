12f3a7f655c0831d78b68a8a81ad4c0e02c6a00a
Owen Garrett <owengarrett@Owens-Air.lan>
2022-12-22 08:44:43 -0500

    added question array

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require("fs")
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {   
+        type: "input" ,
+        message: "What is your project title?" ,
+        name: "title" 
+
+    },
+    {
+        type: "input",
+        message: "What does this application do and who is it intended for?" ,
+        name: "description"
+    },
+    // finish writing questions for prompt
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, generateMarkdown.js)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
