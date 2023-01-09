4bd334f14ea3c13abcba0f3d12d007e4e12ee8b5
Owen Garrett <owengarrett@Owens-Air.lan>
2023-01-09 18:48:44 -0500

    added questions

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ const fs = require("fs")
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {   
-        type: "input" ,
-        message: "What is your project title?" ,
-        name: "title" 
-
-    },
-    {
-        type: "input",
-        message: "What does this application do and who is it intended for?" ,
-        name: "description"
-    },
-    // finish writing questions for prompt
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "Title",
+  },
+  {
+    type: "input",
+    message: "What does this application do and who is it intended for?",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "What command is used to install this app?",
+    name: "Installation",
+  },
+  {
+    type: "input",
+    message: "How is this app intended to be used?",
+    name: "Usage",
+  },
+  // finish writing questions for prompt
 ];
 
 // TODO: Create a function to write README file
@@ -25,7 +34,12 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt (questions).then((inquirerResponses) => {
+    console.log('generating your readMe')
+    writeToFile('readMe.md', generateMarkDown({...inquirerResponses}))
+  })
+};
 
 // Function call to initialize app
 init();
